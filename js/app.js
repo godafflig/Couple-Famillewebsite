7ccf5cc0b276d3cebd7801b8c671d981617cebbb
tom benat <tombenat99@hotmail.fr>
2023-11-18 17:13:04 +0100

    finale commit

--- js/app.js
+++ js/app.js
@@ -140,3 +140,9 @@ function checkScroll() {
     lastScrollY = scrollY;
 }
 
+document.getElementById("ouvrirEmail").addEventListener("click", function(event) {
+    event.preventDefault();
+    var adresseEmail = "couplefamillelyon@wanaddo.fr"; // Remplacez par l'adresse e-mail que vous souhaitez utiliser
+    window.location.href = "mailto:" + adresseEmail;
+});
+
